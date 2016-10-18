85e9ee10713124a4fb7c126b25e2cd671bf8b25e
Kaicheng Yan <kaicheng.yan.1997@gmail.com>
2016-10-17 21:08:13 -0700

    add missing files needed

---
+++ app/src/main/java/me/kyleyan/plumax/BitmapBoardView.java
@@ -0,0 +1,19 @@
+package me.kyleyan.plumax;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class BitmapBoardView extends View {
+    public BitmapBoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
